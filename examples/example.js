42201cb7148720f1c64c9d9c9217a8a565f308bf
xeecos <flashindream@gmail.com>
2017-09-21 13:49:32 +0800

    update

--- examples/example.js
+++ examples/example.js
@@ -1,4 +1,4 @@
-const sorter = require("../index.js");
+const sorter = require("../lib/index.js");
 console.log(sorter.addPath([1, 2.99, 3, 0, 50.2, 6.3]));
 console.log(sorter.addPath([1, 4.99, 3.9, 0, 150.2, 26.3]));
 console.log(sorter.sort());
